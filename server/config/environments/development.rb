2fc20ae0d35a14b4072f289942400a4d514d2767
Eric Kelly <heroiceric@gmail.com>
2014-03-29 14:28:59 -0400

    Hacky method for avoiding CORS issues in dev

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -26,11 +26,4 @@ EmberCareers::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  config.middleware.use Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :options]
-    end
-  end
 end
